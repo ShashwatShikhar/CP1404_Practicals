6888c1404a4146f7d096b01b01322fc25ddf9225
ShashwatShikhar <63352752+ShashwatShikhar@users.noreply.github.com>
2020-04-09 01:44:58 +0530

    print random no.s between a range

---
+++ prac_02/randoms.py
@@ -0,0 +1,11 @@
+import random
+dir(random)
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+print(random.randint(1,101))
+
+print("Ans1:-The smallest no. could be 5 and largest no. could be 19 ")
+print("Ans2:-The smallest no. could be 3 and largest no. could be 9")
+print("Ans3:-The smallest no. could be 2.5 and largest no. could be 5.499999..")
